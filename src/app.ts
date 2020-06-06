8bc7030df9bb1cd2c6313fa85c63ed15f9e37b3c
JorbFreire <jorbfreire@ufrn.edu.br>
2020-06-06 10:47:08 -0300

    simple response to test on heroku

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -13,22 +14,26 @@ createConnection();
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use(cors());
+app.get('/', (require, response) => {
+  return response.status(200).json({ something: 'IfUDontKnowNowUNOW' })
+})
+// app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
+// app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+//   if (err instanceof AppError) {
+//     return response.status(err.statusCode).json({
+//       status: 'error',
+//       message: err.message,
+//     });
+//   }
 
-  console.error(err);
+//   console.error(err);
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+//   return response.status(500).json({
+//     status: 'error',
+//     message: 'Internal server error',
+//   });
+// });
 
 export default app;
